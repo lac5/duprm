3f976ecd1313a86e71c7614ff28da6dac8f50f55
Larry <larry.costigan5@gmail.com>
2021-12-25 18:02:23 -0500

    Add multi-threading

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,3 @@
-macro_rules! try_or_continue {
-    ($x:expr) => {
-        match $x {
-            Ok(x) => x,
-            Err(e) => {
-                eprintln!("ERR {:?}", e);
-                continue;
-            }
-        }
-    };
-}
-
-macro_rules! try_or_log {
-    ($x:expr) => {
-        match $x {
-            Ok(x) => x,
-            Err(e) => {
-                eprintln!("ERR {:?}", e);
-            }
-        }
-    };
-}
-
-macro_rules! some_or_continue {
-    ($x:expr) => {
-        match $x {
-            Some(x) => x,
-            None => continue,
-        }
-    };
-}
-
 extern crate glob;
 extern crate md5;
 extern crate trash;
@@ -39,10 +7,13 @@ mod remove_tags;
 use glob::glob;
 use md5::Digest;
 use remove_tags::remove_tags_from_buffer;
+use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::SystemTime;
 
 struct Row {
@@ -52,39 +23,52 @@ struct Row {
 }
 
 fn main() {
-    let mut found: Vec<Row> = vec![];
+    let found: Arc<Mutex<Vec<Row>>> = Arc::new(Mutex::new(vec![]));
 
     for entry in glob("**/*.mp3").unwrap() {
-        let path = try_or_continue!(entry);
-        let mut f = try_or_continue!(File::open(path.clone()));
-        let mut buffer = Vec::new();
-        try_or_continue!(f.read_to_end(&mut buffer));
-        let buffer = some_or_continue!(remove_tags_from_buffer(buffer));
-        let md5 = md5::compute(buffer);
-        let metadata = try_or_continue!(fs::metadata(path.clone()));
-        let time = try_or_continue!(metadata.created());
+        let found = Arc::clone(&found);
+        thread::spawn(move || {
+            let row = make_row(entry).unwrap();
+            let mut found = found.lock().unwrap();
+            check_found(&mut found, row).unwrap();
+        });
+    }
+}
 
-        println!("{:x} {}", md5, path.display());
+fn make_row(entry: Result<PathBuf, glob::GlobError>) -> Result<Row, Box<dyn Error>> {
+    let path = entry?;
+    let mut f = File::open(path.clone())?;
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer)?;
+    let buffer = remove_tags_from_buffer(buffer).ok_or("Tags could not be removed")?;
+    let md5 = md5::compute(buffer);
+    let metadata = fs::metadata(path.clone())?;
+    let time = metadata.created()?;
 
-        let row1 = Row { time, md5, path };
-        let mut deleted = false;
-        for (i, row2) in found.iter().enumerate() {
-            if row1.md5 == row2.md5 {
-                println!("match ({:x}):", row1.md5);
-                if row1.time > row2.time {
-                    println!("trash#2 -> {}", row2.path.display());
-                    try_or_log!(trash::delete(row2.path.clone()));
-                    found.remove(i);
-                } else {
-                    println!("trash#1 -> {}", row1.path.display());
-                    try_or_log!(trash::delete(row1.path.clone()));
-                    deleted = true;
-                }
-                break;
+    println!("{:x} {}", md5, path.display());
+
+    return Ok(Row { time, md5, path });
+}
+
+fn check_found(found: &mut Vec<Row>, row1: Row) -> Result<(), Box<dyn Error>> {
+    let mut deleted = false;
+    for (i, row2) in found.iter().enumerate() {
+        if row1.md5 == row2.md5 {
+            println!("match ({:x}):", row1.md5);
+            if row1.time > row2.time {
+                println!("trash#2 -> {}", row2.path.display());
+                trash::delete(row2.path.clone())?;
+                found.remove(i);
+            } else {
+                println!("trash#1 -> {}", row1.path.display());
+                trash::delete(row1.path.clone())?;
+                deleted = true;
             }
-        }
-        if !deleted {
-            found.push(row1);
+            break;
         }
     }
+    if !deleted {
+        found.push(row1);
+    }
+    Ok(())
 }
