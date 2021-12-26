46a7fee0f4d6b54e553e64abeeb1245d21b6ff2a
larryc5 <larry.costigan5@gmail.com>
2021-12-26 15:20:58 -0500

    Use HasMap

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use glob::glob;
 use md5::Digest;
 use owo_colors::OwoColorize;
 use remove_tags::remove_tags_from_buffer;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
@@ -14,46 +15,53 @@ use std::sync::{Arc, Mutex};
 use std::time::SystemTime;
 use threadpool::ThreadPool;
 
-struct Row {
-    time: SystemTime,
-    md5: Digest,
-    path: PathBuf,
-}
-
 fn main() {
-    let found: Arc<Mutex<Vec<Row>>> = Arc::new(Mutex::new(vec![]));
+    let mut found = 0;
+    let md5_map = Arc::new(Mutex::new(HashMap::<Digest, (SystemTime, PathBuf)>::new()));
     let pool = ThreadPool::default();
-    let mut jobs = 0;
     let (tx, rx) = channel();
 
     for entry in glob("**/*.mp3").unwrap() {
-        let found = Arc::clone(&found);
+        found += 1;
+        let md5_map = Arc::clone(&md5_map);
         let tx = tx.clone();
-        jobs += 1;
         pool.execute(move || {
-            let do_stuff = || -> Result<(), Box<dyn Error>> {
-                let row = make_row(entry)?;
-                let mut found = found.lock()?;
-                check_found(&mut found, row)?;
-                Ok(())
-            };
-            if let Err(e) = do_stuff() {
-                eprintln!("ERR: {:?}", e);
+            let (md5, time, path) = make_row(entry).unwrap();
+            let mut md5_map = md5_map.lock().unwrap();
+            println!(
+                "{} {:x} {}",
+                md5_map.len(),
+                md5.yellow(),
+                path.display().green()
+            );
+            if let Some((time2, path2)) = md5_map.get(&md5) {
+                println!("match ({:x}):", md5.yellow());
+                if time > *time2 {
+                    println!("trash -> {}", path2.display().blue());
+                    if let Err(e) = trash::delete(path2.clone()) {
+                        eprintln!("{}{:?}", "ERR: ".red(), e);
+                    }
+                    md5_map.insert(md5, (time, path));
+                } else {
+                    println!("trash -> {}", path.display().green());
+                    if let Err(e) = trash::delete(path.clone()) {
+                        eprintln!("{}{:?}", "ERR: ".red(), e);
+                    }
+                }
+            } else {
+                md5_map.insert(md5, (time, path));
             }
-            if let Err(e) = tx.send(1) {
-                eprintln!("ERR: {:?}", e);
-            }
+            tx.send(1).unwrap();
         });
     }
 
-    if jobs != rx.iter().take(jobs).fold(0, |a, b| a + b) {
-        panic!("some jobs didn't finish");
-    } else {
-        println!("done");
-    }
+    println!("{}", rx.iter().take(found).fold(0, |a, b| a + b));
+    println!("done");
 }
 
-fn make_row(entry: Result<PathBuf, glob::GlobError>) -> Result<Row, Box<dyn Error>> {
+fn make_row(
+    entry: Result<PathBuf, glob::GlobError>,
+) -> Result<(Digest, SystemTime, PathBuf), Box<dyn Error>> {
     let path = entry?;
     let mut f = File::open(path.clone())?;
     let mut buffer = Vec::new();
@@ -63,30 +71,5 @@ fn make_row(entry: Result<PathBuf, glob::GlobError>) -> Result<Row, Box<dyn Erro
     let metadata = fs::metadata(path.clone())?;
     let time = metadata.created()?;
 
-    println!("{:x} {}", md5.yellow(), path.display().green());
-
-    return Ok(Row { time, md5, path });
-}
-
-fn check_found(found: &mut Vec<Row>, row1: Row) -> Result<(), Box<dyn Error>> {
-    let mut deleted = false;
-    for (i, row2) in found.iter().enumerate() {
-        if row1.md5 == row2.md5 {
-            println!("match ({:x}):", row1.md5.yellow());
-            if row1.time > row2.time {
-                println!("trash#2 -> {}", row2.path.display().blue());
-                trash::delete(row2.path.clone())?;
-                found.remove(i);
-            } else {
-                println!("trash#1 -> {}", row1.path.display().green());
-                trash::delete(row1.path.clone())?;
-                deleted = true;
-            }
-            break;
-        }
-    }
-    if !deleted {
-        found.push(row1);
-    }
-    Ok(())
+    return Ok((md5, time, path));
 }
