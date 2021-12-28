55ef4c60a0911b4395eba09ce9de55d9ea41b9de
larryc5 <larry.costigan5@gmail.com>
2021-12-27 22:39:13 -0500

    Fix infinite loop

--- src/remove_tags.rs
+++ src/remove_tags.rs
@@ -18,18 +18,21 @@ pub fn remove_tags_from_buffer(data: Buffer) -> Option<Buffer> {
         return None;
     }
 
-    if data.len() >= frame_position + 10 {
+    let len = data.len();
+    if len >= frame_position + 10 {
         let size = decode_size(&data[(frame_position + 6)..(frame_position + 10)]);
-        let mut ret1 = data[0..frame_position].to_vec();
-        let mut ret2 = data[(frame_position + size + 10)..].to_vec();
-        ret1.append(&mut ret2);
-        return Some(ret1);
+        if len >= frame_position + size + 10 {
+            let mut ret1 = data[0..frame_position].to_vec();
+            let mut ret2 = data[(frame_position + size + 10)..].to_vec();
+            ret1.append(&mut ret2);
+            return Some(ret1);
+        }
     }
 
     Some(data)
 }
 
-fn read_u24_be(buffer: &Buffer, offset: usize) -> Option<u32> {
+fn read_u24_be(buffer: &[Byte], offset: usize) -> Option<u32> {
     let len = buffer.len();
     if len - offset < 3 {
         return None;
@@ -41,10 +44,9 @@ fn read_u24_be(buffer: &Buffer, offset: usize) -> Option<u32> {
     Some(sum)
 }
 
-fn is_valid_id3_header(buffer: Buffer) -> bool {
-    !(buffer.len() < 10
-        || read_u24_be(&buffer, 0).iter().any(|&x| x != 0x494433)
-        || !(vec![0x02, 0x03, 0x04].iter().any(|&r| r == buffer[3]))
+fn is_valid_id3_header(buffer: &[Byte]) -> bool {
+    !(read_u24_be(&buffer, 0).iter().any(|&x| x != 0x494433)
+        || !(buffer[3] == 0x02 || buffer[3] == 0x03 || buffer[3] == 0x04)
         || buffer[4] != 0x00
         || buffer[6] & 128 == 1
         || buffer[7] & 128 == 1
@@ -53,19 +55,26 @@ fn is_valid_id3_header(buffer: Buffer) -> bool {
 }
 
 fn get_frame_position(buffer: &Buffer) -> Option<usize> {
-    let mut frame_position = usize::MAX;
+    let mut frame_position: Option<usize>;
+    let mut next_position = 0;
     let mut frame_header_valid = false;
+    let len = buffer.len();
     loop {
-        frame_position = if let Some(x) = buffer[(frame_position + 1)..]
-            .windows(3)
-            .position(|r| r == b"ID3")
-        {
-            frame_header_valid = is_valid_id3_header(buffer[x..(x + 10)].to_vec());
-            x
-        } else {
-            usize::MAX
-        };
-        if !(frame_position != usize::MAX && !frame_header_valid) {
+        frame_position =
+            if let Some(x) = buffer[next_position..].windows(3).position(|r| r == b"ID3") {
+                let x = next_position + x;
+                if x + 10 < len {
+                    let slice = &buffer[x..(x + 10)];
+                    frame_header_valid = is_valid_id3_header(slice);
+                    next_position = x + 3;
+                    Some(x)
+                } else {
+                    None
+                }
+            } else {
+                None
+            };
+        if frame_position == None || frame_header_valid {
             break;
         }
     }
@@ -73,7 +82,7 @@ fn get_frame_position(buffer: &Buffer) -> Option<usize> {
     if !frame_header_valid {
         None
     } else {
-        Some(frame_position)
+        frame_position
     }
 }
 
